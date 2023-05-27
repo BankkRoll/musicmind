0892ee831e4d831e31bb24d952007153df46882b
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-05-26 18:17:22 -0700

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 // src/pages/index.tsx
+
 import { useEffect, useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import axios from 'axios';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import 'animate.css';
+
 
 interface Song {
   albumId: number;
@@ -19,7 +24,7 @@ const Home = () => {
 
   useEffect(() => {
     // Fetch data from a placeholder API
-    axios.get<Song[]>('https://jsonplaceholder.typicode.com/photos?_limit=6')
+    axios.get<Song[]>('https://jsonplaceholder.typicode.com/photos?_limit=10')
       .then(response => {
         setData(response.data);
       })
@@ -37,73 +42,136 @@ const Home = () => {
   return (
     <div className="min-h-screen p-6 bg-gray-800 text-white space-y-8">
 
-        {/* Slideshow Section */}
-        <div className="w-full h-64 overflow-hidden rounded-lg shadow-lg mb-4">
-            <Carousel showThumbs={false} infiniteLoop useKeyboardArrows autoPlay>
-                {data.slice(0,3).map((img, index) => (
-                    <div key={index}>
-                        <img src={img.url} alt={img.title} />
-                    </div>
-                ))}
-            </Carousel>
+      {/* Slideshow Section */}
+      <div className="w-full h-64 overflow-hidden rounded-lg shadow-lg mb-4">
+        <Carousel showThumbs={false} infiniteLoop useKeyboardArrows autoPlay>
+          {data.slice(0, 3).map((img, index) => (
+            <div key={index}>
+              <Link href={`/playlist/[id]?playlistData=${encodeURIComponent(JSON.stringify(img))}`} as={`/playlist/${img.id}`}>
+                <motion.img
+                  src={img.url}
+                  alt={img.title}
+                  whileHover={{ scale: 1.1 }}
+                  transition={{ duration: 0.3 }}
+                />
+              </Link>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+
+      {/* Grid Layout for Trending Playlist and Explore Section */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-4">
+        {/* Trending Playlist Section */}
+        <div className="col-span-2 bg-gray-700 p-6 rounded-lg shadow-lg space-y-4">
+          <h2 className="font-bold text-2xl mb-2 text-white">Trending Playlists</h2>
+          {/* Mock Playlist */}
+          {data.slice(0, 3).map((playlist, index) => (
+            <div
+              key={index}
+              className="flex items-center space-x-4 p-4 rounded-lg shadow-md hover:bg-gray-900"
+            >
+              <Link
+                href={`/playlist/[id]?playlistData=${encodeURIComponent(
+                  JSON.stringify(playlist)
+                )}`}
+                as={`/playlist/${playlist.id}`}
+              >
+                <motion.img
+                  src={playlist.thumbnailUrl}
+                  alt={playlist.title}
+                  className="w-16 h-16 rounded-lg"
+                  whileHover={{ scale: 1.1 }}
+                  transition={{ duration: 0.3 }}
+                />
+              </Link>
+              <div>
+                <h3 className="font-semibold text-lg text-white">{playlist.title}</h3>
+                <p className="text-gray-300">AI generated playlist</p>
+              </div>
+            </div>
+          ))}
         </div>
 
-        {/* Grid Layout for Trending Playlist and Explore Section */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-4">
-            {/* Trending Playlist Section */}
-            <div className="col-span-2 bg-gray-700 p-6 rounded-lg shadow-lg space-y-4">
-                <h2 className="font-bold text-2xl mb-2 text-white">Trending Playlists</h2>
-                {/* Mock Playlist */}
-                {data.slice(0, 3).map((playlist, index) => (
-                    <div key={index} className="flex items-center space-x-4">
-                        <img src={playlist.thumbnailUrl} alt={playlist.title} className="w-16 h-16 rounded-lg" />
-                        <div>
-                            <h3 className="font-semibold text-lg">{playlist.title}</h3>
-                            <p className="text-gray-300">AI generated playlist</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
 
-            {/* Explore Section */}
-            <div className="bg-gray-700 p-6 rounded-lg shadow-lg space-y-4">
-                <h2 className="font-bold text-2xl mb-2 text-white">Explore Now</h2>
-                {/* Search Bar */}
-                <div className="relative">
-                    <input
-                        type="text"
-                        className="w-full pl-10 pr-4 py-2 rounded-lg shadow-md text-black"
-                        placeholder="Search songs..."
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                    />
-                    <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 text-gray-300">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
-                    </div>
+        {/* Explore Section */}
+        <div className="bg-gray-700 p-6 rounded-lg shadow-lg space-y-4">
+          <h2 className="font-bold text-2xl mb-2 text-white">Explore Now</h2>
+          {/* Search Bar */}
+          <div className="relative">
+            <input
+              type="text"
+              className="w-full pl-10 pr-4 py-2 rounded-lg shadow-md text-black"
+              placeholder="Search songs..."
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-6 h-6 text-gray-300"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </div>
+          </div>
+          {/* Sample songs */}
+          {filteredSongs.map((song, index) => (
+            <Link
+              key={index}
+              href={`/playlist/[id]?playlistData=${encodeURIComponent(
+                JSON.stringify(song)
+              )}`}
+              as={`/playlist/${song.id}`}
+            >
+              <motion.div
+                className="flex items-center space-x-4 p-4 rounded-lg shadow-md hover:bg-gray-900"
+                whileHover={{ scale: 1.01 }}
+                transition={{ duration: 0.3 }}
+              >
+                <img
+                  src={song.thumbnailUrl}
+                  alt={song.title}
+                  className="w-12 h-12 rounded-lg"
+                />
+                <div>
+                  <h3 className="font-semibold text-lg text-white">{song.title}</h3>
+                  <p className="text-gray-300">AI generated song</p>
                 </div>
-                {/* Sample songs */}
-                {filteredSongs.map((song, index) => (
-                    <div key={index} className="flex items-center space-x-4">
-                        <img src={song.thumbnailUrl} alt={song.title} className="w-12 h-12 rounded-lg" />
-                        <div>
-                            <h3 className="font-semibold text-lg">{song.title}</h3>
-                            <p className="text-gray-300">AI generated song</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
+              </motion.div>
+            </Link>
+          ))}
         </div>
+      </div>
 
-        {/* Other Sections */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-          {data.slice(3).map((item, index) => (
-            <div key={index} className="bg-gray-700 p-6 rounded-lg shadow-lg">
-                <h2 className="font-bold text-2xl mb-2 text-white">{item.title}</h2>
-                {/* Content for other sections */}
-                <img src={item.thumbnailUrl} alt={item.title} className="rounded-lg mt-2"/>
-                <p className="text-gray-300 mt-2">This is some additional text related to the image above.</p>
-            </div>
+      {/* Other Sections */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {data.slice(3).map((item, index) => (
+          <div key={index} className="bg-gray-700 p-6 rounded-lg shadow-lg">
+            <h2 className="font-bold text-2xl mb-2 text-white">
+              <Link href={`/playlist/[id]?playlistData=${encodeURIComponent(JSON.stringify(item))}`} as={`/playlist/${item.id}`}>
+                {item.title}
+              </Link>
+            </h2>
+            {/* Content for other sections */}
+            <Link href={`/playlist/[id]?playlistData=${encodeURIComponent(JSON.stringify(item))}`} as={`/playlist/${item.id}`}>
+              <motion.img
+                src={item.thumbnailUrl}
+                alt={item.title}
+                className="rounded-lg mt-2"
+                whileHover={{ scale: 1.1 }}
+                transition={{ duration: 0.3 }}
+              />
+            </Link>
+            <p className="text-gray-300 mt-2">This is some additional text related to the image above.</p>
+          </div>
         ))}
       </div>
     </div>
--- src/pages/playlist/[id].tsx
+++ src/pages/playlist/[id].tsx
@@ -1,21 +1,21 @@
 // src/pages/playlist/[id].tsx
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { motion } from 'framer-motion';
 
-interface PlaylistData {
-  id: string;
-  name: string;
-  songs: string[];
+interface Song {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
 }
 
-// Fetch playlist data from an API (this is a placeholder)
-const fetchPlaylistData = async (id: string): Promise<PlaylistData> => {
-  return {
-    id,
-    name: 'Playlist ' + id,
-    songs: ['Song 1', 'Song 2', 'Song 3'],
-  };
-};
+interface PlaylistData {
+  name: string;
+  songs: Song[];
+}
 
 const Playlist = () => {
   const router = useRouter();
@@ -23,28 +23,92 @@ const Playlist = () => {
   const [playlistData, setPlaylistData] = useState<PlaylistData | null>(null);
 
   useEffect(() => {
-    if (id && typeof id === 'string') {
-      fetchPlaylistData(id).then(data => setPlaylistData(data));
+    if (id) {
+      // Fetch playlist data from the API based on the ID
+      axios
+        .get<Song[]>(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`)
+        .then(response => {
+          setPlaylistData({
+            name: `Playlist ${id}`,
+            songs: response.data,
+          });
+        })
+        .catch(error => {
+          console.log(error);
+          setPlaylistData(null);
+        });
     }
   }, [id]);
 
-  return (
-    <div className="p-6">
-      {playlistData ? (
-        <div>
-          <h1 className="text-3xl font-bold mb-4">{playlistData.name}</h1>
-          <ul>
+return (
+  <div className="p-6">
+    {playlistData ? (
+      <div>
+        <motion.h1
+          className="text-3xl font-bold mb-4"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          {playlistData.name}
+        </motion.h1>
+        <div className="overflow-x-auto">
+          <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {playlistData.songs.map((song, index) => (
-              <li key={index}>{song}</li>
+              <motion.li
+                key={index}
+                className="flex items-center space-x-4 bg-gray-800 p-4 rounded-lg shadow-lg hover:shadow-2xl"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+              >
+                <motion.img
+                  src={song.thumbnailUrl}
+                  alt={song.title}
+                  className="w-16 h-16 rounded-lg"
+                  whileHover={{ scale: 1.1 }}
+                />
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: index * 0.1 + 0.2 }}
+                >
+                  <motion.h3
+                    className="font-semibold text-lg text-white"
+                    whileHover={{ scale: 1.05 }}
+                  >
+                    {song.title}
+                  </motion.h3>
+                  <motion.p
+                    className="text-gray-300"
+                    whileHover={{ scale: 1.05 }}
+                  >
+                    AI generated song
+                  </motion.p>
+                </motion.div>
+              </motion.li>
             ))}
           </ul>
         </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+      </div>
+    ) : (
+      <motion.div
+        className="flex items-center justify-center h-64"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <motion.p
+          className="text-2xl text-gray-400"
+          whileHover={{ scale: 1.05 }}
+        >
+          Loading...
+        </motion.p>
+      </motion.div>
+    )}
+  </div>
+);
+
 };
 
 export default Playlist;
-
